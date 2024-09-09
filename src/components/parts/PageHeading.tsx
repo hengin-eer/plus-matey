b976fafb8faf1aab393c223c8450a81ea2b58d88
hengin-eer <hirokix0608@gmail.com>
2024-09-09 19:10:52 +0900

    fix: 文字を少し右寄せ

--- src/components/parts/PageHeading.tsx
+++ src/components/parts/PageHeading.tsx
@@ -1,4 +1,4 @@
-import { FC, HTMLAttributes } from 'react';
+import { FC } from 'react';
 
 type Props = {
 	children: string;
@@ -7,7 +7,7 @@ type Props = {
 
 const PageHeading: FC<Props> = ({ children, className }) => {
 	return (
-		<div className="w-full pb-2 mb-5 border-b-2 border-gray">
+		<div className="w-full pl-2 pb-2 mb-5 border-b-2 border-gray">
 			<h2 className={`text-2xl text-black ${className}`}>{children}</h2>
 		</div>
 	);
