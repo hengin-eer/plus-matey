6f5cd76e537a522c073091d3648a46be5f461497
hengin-eer <hirokix0608@gmail.com>
2024-09-12 10:22:37 +0900

    feat: 注意書きの追加、スタイルの修正、検索機能のメモ

--- src/components/sections/SectionSearchEvents.tsx
+++ src/components/sections/SectionSearchEvents.tsx
@@ -1,3 +1,6 @@
+// TODO: 後で検索機能の部分的なクライアントコンポ化を検討する
+'use client';
+
 import { FC } from 'react';
 import PageHeading from '../parts/PageHeading';
 import { Icon } from '@iconify/react';
@@ -26,6 +29,11 @@ const SectionSearchEvents: FC = () => {
 		},
 	];
 
+	// TODO: 検索ワードと検索条件をクエリパラメータを使ってイベントを検索する処理
+	const handleSearchEvents = () => {
+		alert('検索します～');
+	};
+
 	return (
 		// w-[360px] は一時的な措置。子要素のインプットフォームの大きさ基準でレスポンシブ対応
 		<section className="w-[360px]">
@@ -39,7 +47,11 @@ const SectionSearchEvents: FC = () => {
 						className="px-5 py-3 rounded-lg bg-light-gray placeholder:text-base placeholder:
 					text-gray focus-visible:text-foreground focus-visible:outline-gray transition-all duration-300"
 					/>
-					<button className="flex items-center justify-center gap-2 h-12 rounded-lg bg-primary-green">
+					<button
+						type="submit"
+						onClick={() => handleSearchEvents()}
+						className="flex items-center justify-center gap-2 h-12 rounded-lg bg-primary-green"
+					>
 						<Icon
 							icon="material-symbols-light:search-rounded"
 							className="size-8"
@@ -48,27 +60,31 @@ const SectionSearchEvents: FC = () => {
 					</button>
 				</div>
 
-				<div className="flex flex-col gap-8">
-					{/* TODO: ラジオボタンのスタイル追加 */}
+				<div className="flex flex-col gap-6">
 					{searchOptions.map((item) => (
-						<label key={item.id} className="relative flex items-center gap-6">
+						<label
+							key={item.id}
+							className="relative flex items-center gap-4 select-none"
+						>
 							<input
 								type="checkbox"
 								name="search-options"
 								value={item.id}
 								className="hidden peer/checkbox"
 							/>
-							<span className="relative size-5 border border-black rounded-[4px] peer-checked/checkbox:bg-primary-yellow transition-all" />
+							<span className="relative size-4 border border-black rounded-[4px] peer-checked/checkbox:bg-primary-yellow transition-all" />
 							<Icon
 								icon="material-symbols-light:check-small-rounded"
-								className="absolute -left-[6px] opacity-0 size-8 peer-checked/checkbox:opacity-100 transition-all"
+								className="absolute -left-[4px] opacity-0 size-6 peer-checked/checkbox:opacity-100 transition-all"
 							/>
 							<p>{item.jp}</p>
 						</label>
 					))}
 				</div>
 
-				{/* TODO: 注意書き等々を追加... */}
+				<p className="text-sm leading-6">
+					「締め切りが間近」なイベントは1週間以内に募集が締め切られます。
+				</p>
 			</div>
 		</section>
 	);
