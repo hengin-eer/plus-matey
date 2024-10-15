9bb1cf08492eba42c13836a2e77c1fc973ea797a
hengin-eer <hirokix0608@gmail.com>
2024-10-16 01:05:12 +0900

    ✨ #17 ページに各セクションを追加

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,8 +1,25 @@
+import SectionHeading from '@/components/sections/create/SectionHeading';
+import SectionLeftForms from '@/components/sections/create/SectionLeftForms';
+import SectionRightForms from '@/components/sections/create/SectionRightForms';
+
 function Create() {
 	return (
-		<div>
-			<p>Create</p>
-		</div>
+		<main className="px-10">
+			<form className="pt-5 pb-20 flex flex-col gap-10">
+				{/* 
+				NOTE: Server Actionを使って実装
+				TODO: 暫定的に固定幅にしている。後でレスポンシブの設定をする
+				 */}
+
+				<SectionHeading />
+
+				{/* TODO: レスポンシブ対応しましょう！！ */}
+				<div className="w-full flex justify-center gap-6">
+					<SectionLeftForms />
+					<SectionRightForms />
+				</div>
+			</form>
+		</main>
 	);
 }
 
