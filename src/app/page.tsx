a7a7f21e1e972d1f25b1b2ff79c16c212874365d
kiyuasusili <tsujimoto@growth-verse.ai>
2024-10-16 00:13:10 +0900

    feat: ホームページへのイベントリストの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import SectionNewEventList from '@/components/sections/SectionNewEventList';
 import SectionSearchEvents from '@/components/sections/SectionSearchEvents';
 
 export default function Home() {
 	return (
-		<main className="mt-10 px-10">
+		<main className="flex gap-[100px] mt-10 px-10">
 			<SectionSearchEvents />
+			<SectionNewEventList />
 		</main>
 	);
 }
