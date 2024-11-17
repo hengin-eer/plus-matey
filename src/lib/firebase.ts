787dd624374bf58cb68614df84a93302be112405
hengin-eer <hirokix0608@gmail.com>
2024-11-17 21:54:23 +0900

    ✨ #18 FBの初期化を更新

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -22,7 +22,7 @@ export const firestore = initFirestore({
 	credential: cert({
 		projectId: process.env.FB_PROJECT_ID,
 		clientEmail: process.env.FB_CLIENT_EMAIL,
-		privateKey: process.env.FB_PRIVATE_KEY,
+		privateKey: process.env.FB_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 	}),
 });
 
