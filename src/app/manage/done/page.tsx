7dfdfb8dde9fa146cc569fbe9ebd72759213dac5
hengin-eer <hirokix0608@gmail.com>
2024-11-26 03:20:49 +0900

    ✨ #18 とりまレイアウト作成＆編集完了後のページ作成

---
+++ src/app/manage/done/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+
+function Done() {
+	const searchParams = useSearchParams();
+
+	const message = searchParams.get('message');
+
+	return (
+		<div className="pt-20 grid place-items-center">
+			<div className="flex flex-col items-center gap-7 py-8 px-10 bg-white rounded-lg shadow-md">
+				<p className="text-lg">{message ? message : '不正なアクセスです。'}</p>
+				<div className="flex flex-col gap-4">
+					<Link
+						href="/"
+						className="py-3 px-5 rounded-md bg-light-gray text-center"
+					>
+						トップへ戻る
+					</Link>
+					<Link
+						href="/manage"
+						className="py-3 px-5 rounded-md bg-primary-green text-center"
+					>
+						イベント管理ページへ
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Done;
