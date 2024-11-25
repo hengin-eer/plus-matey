e65dd84113929e2e7f8f93336a1c73a08126a684
hengin-eer <hirokix0608@gmail.com>
2024-11-26 03:22:19 +0900

    🔧 #18 adminのインポート方法の変更など、フロント用のfirebaseのコメントアウト

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,21 +1,29 @@
 import { initFirestore } from '@auth/firebase-adapter';
-import { cert } from 'firebase-admin/app';
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import * as admin from 'firebase-admin';
+import { cert, getApps } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 
-const firebaseConfig = {
-	apiKey: process.env.FB_API_KEY,
-	authDomain: process.env.FB_AUTH_DOMAIN,
-	projectId: process.env.FB_PROJECT_ID,
-	storageBucket: process.env.FB_STORAGE_BUCKET,
-	messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-	appId: process.env.FB_APPID,
-	measurementId: process.env.FB_MEASUREMENT_ID,
-};
+// const firebaseConfig = {
+// 	apiKey: process.env.FB_API_KEY,
+// 	authDomain: process.env.FB_AUTH_DOMAIN,
+// 	projectId: process.env.FB_PROJECT_ID,
+// 	storageBucket: process.env.FB_STORAGE_BUCKET,
+// 	messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+// 	appId: process.env.FB_APPID,
+// 	measurementId: process.env.FB_MEASUREMENT_ID,
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const firebaseAuth = getAuth(app);
+if (!getApps()?.length) {
+	admin.initializeApp({
+		credential: cert({
+			projectId: process.env.FB_PROJECT_ID,
+			clientEmail: process.env.FB_CLIENT_EMAIL,
+			privateKey: process.env.FB_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+		}),
+	});
+}
+
+export const db = getFirestore();
 
 // Auth.js用
 export const firestore = initFirestore({
@@ -25,5 +33,3 @@ export const firestore = initFirestore({
 		privateKey: process.env.FB_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 	}),
 });
-
-export { firebaseAuth };
