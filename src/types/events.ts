88a27de5583cba67bec41c0e6fe8ed0b3262f433
hengin-eer <hirokix0608@gmail.com>
2024-11-26 14:39:02 +0900

    🏷️ #18 イベント情報の型定義

---
+++ src/types/events.ts
@@ -0,0 +1,20 @@
+export interface EventData {
+	id: string;
+	title: string;
+	summary: string;
+	thumbnail?: {
+		base64: string;
+		name: string;
+	} | null;
+	close_at: string;
+	number_recruited: number;
+	held_at: string;
+	department: string[];
+	sex: string;
+	grade: string[];
+	is_public: boolean;
+	author_id: string;
+	created_at: Date;
+	updated_at: Date;
+	// その他オーナー情報も追加しよう
+}
